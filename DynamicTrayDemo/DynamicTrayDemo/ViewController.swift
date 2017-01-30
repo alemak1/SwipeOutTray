2964a0b3a12100ad958d624e4da57f890951ab7a
yongrenwang <czie.alexmak@gmail.com>
2017-01-30 17:48:41 +0800

    Implemented helper methods to setup the trayView and to add gesture recognizers to both the tray view and the right screen edge

--- DynamicTrayDemo/DynamicTrayDemo/ViewController.swift
+++ DynamicTrayDemo/DynamicTrayDemo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var trayView: UIVisualEffectView = UIVisualEffectView()
+    var trayLeftEdgeConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,8 +28,62 @@ class ViewController: UIViewController {
             imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             
             ])
+        
+        setupTrayView()
+        setupGestureRecognizers()
+    }
+    
+    
+    func setupGestureRecognizers(){
+        let edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(pan))
+        edgePan.edges = UIRectEdge.right
+        view.addGestureRecognizer(edgePan)
+        
+        let trayPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        trayView.addGestureRecognizer(trayPanGestureRecognizer)
+        
+    }
+    
+    func pan(){
+        
     }
 
+    func setupTrayView(){
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+        trayView = UIVisualEffectView(effect: blurEffect)
+        trayView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(trayView)
+
+        NSLayoutConstraint.activate([
+            trayView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            trayView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            trayView.topAnchor.constraint(equalTo: view.topAnchor)
+            ])
+        
+
+        
+        trayLeftEdgeConstraint = NSLayoutConstraint(item: trayView, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: view.frame.size.width)
+        
+        trayLeftEdgeConstraint?.isActive = true
+        
+        let trayLabel = UILabel()
+        trayLabel.text = "Good morning, \n Sunshine \n, Maybel the Frenchie"
+        trayLabel.numberOfLines = 0
+        trayLabel.font = UIFont.boldSystemFont(ofSize: 24.0)
+        trayLabel.translatesAutoresizingMaskIntoConstraints = false
+        trayView.addSubview(trayLabel)
+        
+        NSLayoutConstraint.activate([
+            trayLabel.leftAnchor.constraint(equalTo: trayView.leftAnchor, constant: 30),
+            trayLabel.rightAnchor.constraint(equalTo: trayView.rightAnchor, constant: 30),
+            trayLabel.topAnchor.constraint(equalTo: trayView.topAnchor, constant: 100),
+            trayLabel.bottomAnchor.constraint(equalTo: trayView.bottomAnchor, constant: 100)
+            ])
+        
+        view.layoutIfNeeded()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
